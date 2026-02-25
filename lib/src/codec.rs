2bea2efcc77052880e6cdb12c2c92f9bcc636b41
Richard Neumann <mail@richard-neumann.de>
2026-02-25 21:15:07 +0100

    Decouple `Codec` from `Encoder` and `Decoder`.

--- lib/src/codec.rs
+++ lib/src/codec.rs
@@ -2,23 +2,31 @@ pub use crate::decoder::Decoder;
 pub use crate::encoder::Encoder;
 
 /// Gives the ability to en- / decode bytes to / from ANSI background colors.
-pub trait Codec: Encoder + Decoder {
+pub trait Codec {
+    /// Type to encode bytes into ANSI color codes.
+    type Encoder;
+    /// A type to decode color codes.
+    type Decoder;
+
     /// Encode bytes into ANSI colors.
-    fn encode(self) -> <Self as Encoder>::Encoder;
+    fn encode(self) -> Self::Encoder;
 
     /// Decode ANSI color codes into bytes.
-    fn decode(self) -> <Self as Decoder>::Decoder;
+    fn decode(self) -> Self::Decoder;
 }
 
 impl<T> Codec for T
 where
     T: Encoder + Decoder,
 {
-    fn encode(self) -> <Self as Encoder>::Encoder {
+    type Encoder = <Self as Encoder>::Encoder;
+    type Decoder = <Self as Decoder>::Decoder;
+
+    fn encode(self) -> Self::Encoder {
         <Self as Encoder>::encode(self)
     }
 
-    fn decode(self) -> <Self as Decoder>::Decoder {
+    fn decode(self) -> Self::Decoder {
         <Self as Decoder>::decode(self)
     }
 }
